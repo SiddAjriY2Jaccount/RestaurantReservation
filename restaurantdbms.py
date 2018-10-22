32f900a885706ccfd65c16613e3c7d80d127027e
SiddAjriY2Jaccount <y2jsiddajri@gmail.com>
2018-10-22 15:32:47 +0530

    classes used

--- restaurantdbms.py
+++ restaurantdbms.py
@@ -1,66 +1,61 @@
 import pymysql
 from tkinter import *
+conn = pymysql.connect(host='localhost', port=3306, user='sidd', passwd='123', db='reservations')
+#cur = conn.cursor()
+
+class Login:
+    def __init__(self, f1):
+        self.f1=f1;
+        self.Username=StringVar()
+        self.Password=StringVar()
+        self.Email = StringVar()
+
+        self.label_0 = Label(f1, text="Login",width=20,font=("bold", 20))
+        self.label_0.place(x=90,y=53)
+
+        self.label_1 = Label(f1, text="Username",width=20,font=("bold", 10))
+        self.label_1.place(x=80,y=130)
+
+        self.entry_1 = Entry(f1, textvar=self.Username)
+        self.entry_1.place(x=240,y=130)
+
+        self.label_2 = Label(f1, text="Password",width=20,font=("bold", 10))
+        self.label_2.place(x=68,y=180)
+
+        self.entry_2 = Entry(f1, textvar=self.Password)
+        self.entry_2.place(x=240,y=180)
+
+        self.label_3 = Label(f1, text="Email",width=20,font=("bold", 10))
+        self.label_3.place(x=70,y=230)
+
+        self.entry_3 = Entry(f1, textvar=self.Email)
+        self.entry_3.place(x=240,y=230)
+
+        self.btn = Button(f1, text='Submit',width=20,bg='dark blue',fg='white', command=self.insertlogin).place(x=180,y=300)
+
+    def insertlogin(self):
+        name=self.Username.get()
+        email=self.Email.get()
+        password=self.Password.get()
+           #conn = pymysql.connect(host='localhost', port=3306, user='sidd', passwd='123', db='reservations')
+        cur = conn.cursor()
+           #cur.execute('CREATE TABLE IF NOT EXISTS Student (Fullname TEXT,Email TEXT,Gender TEXT,country TEXT,Programming TEXT)')
+        sql1 = "INSERT INTO `users` (`username`,`password`,`email`) VALUES (%s, %s, %s)"
+        cur.execute(sql1, (name, password, email))
+        conn.commit()
+        cur.close()
+        self.f1.destroy()
+        #global root
+        f2 = Frame(root,width=500,height=500)
+        f2.pack()
+        Lab = Label(f2, text= "Test")
+        Lab.pack()
+
 
 root = Tk()
-
-conn = pymysql.connect(host='localhost', port=3306, user='sidd', passwd='123', db='reservations')
-cur = conn.cursor()
-#cur.execute("SELECT * FROM restaurant_details")
-
-#print(cur.description)
-#print()
-
-
-#for row in cur:
-    #print(row)
-    #label = Label(root, text=row)
-    #label.pack()
-
-
 root.geometry('500x500')
 root.title("Login")
-
-
-
-Username=StringVar()
-Password=StringVar()
-Email = StringVar()
-
-def insertlogin():
-   name=Username.get()
-   email=Email.get()
-   password=Password.get()
-   #conn = pymysql.connect(host='localhost', port=3306, user='sidd', passwd='123', db='reservations')
-   #cur = conn.cursor()
-   #cur.execute('CREATE TABLE IF NOT EXISTS Student (Fullname TEXT,Email TEXT,Gender TEXT,country TEXT,Programming TEXT)')
-   sql1 = "INSERT INTO `users` (`username`,`password`,`email`) VALUES (%s, %s, %s)"
-   cur.execute(sql1, (name, password, email))
-   conn.commit()
-   cur.close()
-
-
-label_0 = Label(root, text="Login",width=20,font=("bold", 20))
-label_0.place(x=90,y=53)
-
-label_1 = Label(root, text="Username",width=20,font=("bold", 10))
-label_1.place(x=80,y=130)
-
-entry_1 = Entry(root, textvar=Username)
-entry_1.place(x=240,y=130)
-
-label_2 = Label(root, text="Password",width=20,font=("bold", 10))
-label_2.place(x=68,y=180)
-
-entry_2 = Entry(root, textvar=Password)
-entry_2.place(x=240,y=180)
-
-label_3 = Label(root, text="Email",width=20,font=("bold", 10))
-label_3.place(x=70,y=230)
-
-entry_3 = Entry(root, textvar=Email)
-entry_3.place(x=240,y=230)
-
-Button(root, text='Submit',width=20,bg='dark blue',fg='white', command=insertlogin).place(x=180,y=300)
-
-
+f1 = Frame(root,width=500,height=500)
+f1.pack()
+login = Login(f1)
 root.mainloop()
